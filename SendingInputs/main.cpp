42369d2343ea58e51d44a1322a0ebe1e2b54fd60
Asperatology <tom.mai78101@gmail.com>
2015-02-18 00:31:56 -0500

    Truly the initial commit.
    May be the final commit for this repository. Trivial to update the inputs though.

---
+++ SendingInputs/main.cpp
@@ -0,0 +1,92 @@
+#include <Windows.h>
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+
+//Converting chars to virtual codes
+static HKL layout = GetKeyboardLayout(0);
+
+
+void GenerateKey(int vk, bool extended){
+	KEYBDINPUT keyboard = {};
+	INPUT input = {};
+
+	//Key down
+	if (extended){
+		keyboard.dwFlags = KEYEVENTF_EXTENDEDKEY;
+	}
+	keyboard.wVk = vk;
+	input.type = INPUT_KEYBOARD;
+	input.ki = keyboard;
+	SendInput(1, &input, sizeof(input));
+
+	//Key up
+	keyboard = {};
+	input = {};
+	keyboard.dwFlags = KEYEVENTF_KEYUP;
+	if (extended){
+		keyboard.dwFlags |= KEYEVENTF_EXTENDEDKEY;
+	}
+	keyboard.wVk = vk;
+	input.type = INPUT_KEYBOARD;
+	input.ki = keyboard;
+	SendInput(1, &input, sizeof(input));
+	return;
+}
+
+
+void GenerateStrings(char* str, int size){
+	for (int i = 0; i < size; i++){
+		short vk = VkKeyScanEx(str[i], layout);
+		GenerateKey(vk, false);
+	}
+	GenerateKey(VK_RETURN, false);
+}
+
+void main(){
+	srand(time(0));
+	int r_old = -1, r;
+	while (true){
+		//***   PLEASE CHANGE THIS TO WHATEVER FAVORITE BROWSER YOU ARE USING.
+		//***   ALSO PLEASE MODIFY THE STRING VALUE IF THE TITLE OF YOUR TAB IS WRONG.
+		//HWND window = FindWindow(0, "TwitchPlaysPokemon - Twitch - Google Chrome");
+		//HWND window = FindWindow(0, "TwitchPlaysPokemon - Twitch - Mozilla Firefox");
+		HWND window = FindWindow(0, "TwitchPlaysPokemon - Twitch - Internet Explorer");
+		if (window){
+			SetForegroundWindow(window);
+			SetFocus(window);
+		}
+		r = rand() % 6;
+		if (r_old != r){
+			switch (r){
+				case 0:{
+					GenerateStrings("down", 4);
+					break;
+				}
+				case 1:{
+					GenerateStrings("up", 2);
+					break;
+				}
+				case 2:{
+					GenerateStrings("left", 4);
+					break;
+				}
+				case 3:{
+					GenerateStrings("right", 5);
+					break;
+				}
+				case 4:{
+					GenerateStrings("a", 1);
+					break;
+				}
+				case 5:{
+					GenerateStrings("b", 1);
+					break;
+				}
+			}
+			Sleep(5015);
+		}
+		r_old = r;
+	}
+}
