b06260e3af11d4f36c405e82fc63926c3f042fd9
Asperatology <tom.mai78101@gmail.com>
2015-02-18 13:46:39 -0500

    Fixed logic errors.

--- SendingInputs/main.cpp
+++ SendingInputs/main.cpp
@@ -40,13 +40,15 @@ void GenerateStrings(char* str, int size){
 	for (int i = 0; i < size; i++){
 		short vk = VkKeyScanEx(str[i], layout);
 		GenerateKey(vk, false);
+		Sleep(100);
 	}
 	GenerateKey(VK_RETURN, false);
 }
 
 void main(){
 	srand(time(0));
-	int r_old = -1, r;
+	int r_old = -1, r = -2;
+	bool flag = false;
 	while (true){
 		//***   PLEASE CHANGE THIS TO WHATEVER FAVORITE BROWSER YOU ARE USING.
 		//***   ALSO PLEASE MODIFY THE STRING VALUE IF THE TITLE OF YOUR TAB IS WRONG.
@@ -57,36 +59,36 @@ void main(){
 			SetForegroundWindow(window);
 			SetFocus(window);
 		}
-		r = rand() % 6;
-		if (r_old != r){
-			switch (r){
-				case 0:{
-					GenerateStrings("down", 4);
-					break;
-				}
-				case 1:{
-					GenerateStrings("up", 2);
-					break;
-				}
-				case 2:{
-					GenerateStrings("left", 4);
-					break;
-				}
-				case 3:{
-					GenerateStrings("right", 5);
-					break;
-				}
-				case 4:{
-					GenerateStrings("a", 1);
-					break;
-				}
-				case 5:{
-					GenerateStrings("b", 1);
-					break;
-				}
+		while (r == r_old){
+			r = rand() % 3;
+		}
+		switch (r){
+			case 0:{
+				GenerateStrings("right", 5);
+				break;
 			}
-			Sleep(5015);
+			case 1:{
+				GenerateStrings("up", 2);
+				break;
+			}
+			case 2:{
+				GenerateStrings("a", 1);
+				break;
+			}
+			case 3:{
+				GenerateStrings("left", 4);
+				break;
+			}
+			case 4:{
+				GenerateStrings("down", 4);
+				break;
+			}
+			case 5:{
+				GenerateStrings("b", 1);
+				break;
+			}
 		}
 		r_old = r;
+		Sleep(5015 + ((r + 3) * 5));
 	}
 }
